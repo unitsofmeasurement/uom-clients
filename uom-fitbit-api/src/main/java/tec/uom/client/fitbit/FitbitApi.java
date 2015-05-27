dc0e6e5122977471bd248fdb94bdac41716a7d90
Werner Keil <werner.keil@gmx.net>
2015-05-27 20:58:26 +0200

    8: Start CDI implementation
    Task-Url:
    https://github.com/unitsofmeasurement/uom-fitbit/issues/issues/issue/8

--- uom-fitbit-api/src/main/java/tec/uom/client/fitbit/FitbitApi.java
+++ uom-fitbit-api/src/main/java/tec/uom/client/fitbit/FitbitApi.java
@@ -16,45 +16,80 @@
 
 package tec.uom.client.fitbit;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import org.agorava.api.oauth.application.OAuthAppSettings;
 import org.agorava.api.service.OAuthEncoder;
 import org.agorava.api.service.Preconditions;
 import org.agorava.spi.ProviderConfigOauth20Final;
 
-	/**
-	 * @author Werner Keil
-	 * Date: 27/05/15
-	 * Time: 20:35
-	 */
-	@Fitbit
-	public class FitbitApi extends ProviderConfigOauth20Final {
-		private static final String DEFAULT_API_BASE_URL = "api.fitbit.com";
-		
-	    private static final String MEDIA_NAME = "Fitbit";
-	    private static final String AUTHORIZE_URL = "https://instagram.com/oauth/authorize/?client_id=%s&redirect_uri=%s&response_type=token";
-	    private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL + "&scope=%s";
+/**
+ * @author Werner Keil Date: 27/05/15 Time: 20:35
+ */
+@Fitbit
+public class FitbitApi extends ProviderConfigOauth20Final {
 
-	    @Override
-	    public String getAccessTokenEndpoint() {
-	        //return "https://" + apiBaseUrl + "/oauth/request_token"";
-	    	return "https://" + DEFAULT_API_BASE_URL + "/oauth/request_token";
-	    }
+	public static enum Version {
 
-	    @Override
-	    public String getAuthorizationUrl(OAuthAppSettings oAuthAppSettings) {
-	        Preconditions.checkValidUrl(oAuthAppSettings.getCallback(), "Must provide a valid url as callback. Instagram does not support OOB");
+		BETA_1;
 
-	        // Append scope if present
-	        if (oAuthAppSettings.hasScope()) {
-	            return String.format(SCOPED_AUTHORIZE_URL, oAuthAppSettings.getApiKey(), OAuthEncoder.encode(oAuthAppSettings.getCallback()), OAuthEncoder.encode(oAuthAppSettings.getScope()));
-	        } else {
-	            return String.format(AUTHORIZE_URL, oAuthAppSettings.getApiKey(), OAuthEncoder.encode(oAuthAppSettings.getCallback()));
-	        }
-	    }
+		private String version;
 
-	    @Override
-	    public String getProviderName() {
-	        return MEDIA_NAME;
-	    }
+		private Version() {
+			version = name().replaceAll("^.*_([^_]+)$", "$1");
+		}
+
+		public String getVersion() {
+			return version;
+		}
+
+		public static Version fromVersion(String v) {
+			return Version.valueOf("BETA_" + v);
+		}
 	}
 
+	private static final String DEFAULT_API_BASE_URL = "api.fitbit.com";
+	private static final String DEFAULT_WEB_BASE_URL = "https://www.fitbit.com";
+	protected static final String SUBSCRIBER_ID_HEADER_NAME = "X-Fitbit-Subscriber-Id";
+
+	private SimpleDateFormat format = new SimpleDateFormat(
+			"EEE, d MMM yyyy HH:mm:ss z", Locale.ENGLISH);
+	private String apiBaseUrl = DEFAULT_API_BASE_URL;
+	private Version apiVersion = Version.BETA_1;
+
+	private static final String MEDIA_NAME = "Fitbit";
+	//private static final String AUTHORIZE_URL = "https://instagram.com/oauth/authorize/?client_id=%s&redirect_uri=%s&response_type=token";
+	private static final String AUTHORIZE_URL = "https://www.fitbit.com/oauth/authorize";
+	private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL
+			+ "&scope=%s";
+
+	@Override
+	public String getAccessTokenEndpoint() {
+		//return "https://" + apiBaseUrl + "/oauth/request_token";
+		return "https://api.fitbit.com/oauth/access_token";
+	}
+
+	@Override
+	public String getAuthorizationUrl(OAuthAppSettings oAuthAppSettings) {
+		Preconditions
+				.checkValidUrl(oAuthAppSettings.getCallback(),
+						"Must provide a valid url as callback. Instagram does not support OOB");
+
+		// Append scope if present
+		/* if (oAuthAppSettings.hasScope()) {
+			return String.format(SCOPED_AUTHORIZE_URL,
+					oAuthAppSettings.getApiKey(),
+					OAuthEncoder.encode(oAuthAppSettings.getCallback()),
+					OAuthEncoder.encode(oAuthAppSettings.getScope()));
+		} else { */
+			return String.format(AUTHORIZE_URL, oAuthAppSettings.getApiKey(),
+					OAuthEncoder.encode(oAuthAppSettings.getCallback()));
+		//}
+	}
+
+	@Override
+	public String getProviderName() {
+		return MEDIA_NAME;
+	}
+}
